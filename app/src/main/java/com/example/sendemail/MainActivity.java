123053d1f96a4f86e0806e9f72ba3bc154999040
johantrieu <johantrieu@gmail.com>
2020-11-07 17:16:59 +0200

    initial commit

---
+++ app/src/main/java/com/example/sendemail/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.sendemail;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eTo;
+    private EditText eSubject;
+    private EditText eMsg;
+    private Button btn;
+
+    ImageButton voice_btn;
+    final int VOICE_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        eTo = (EditText)findViewById(R.id.textTo);
+        eSubject = (EditText)findViewById(R.id.textSubject);
+        eMsg = (EditText)findViewById(R.id.textMessage);
+        btn = (Button)findViewById(R.id.sendEmail);
+        voice_btn = (ImageButton) findViewById(R.id.voice_btn);
+        btn.setOnClickListener(v -> {
+            Intent it = new Intent(Intent.ACTION_SEND);
+            it.putExtra(Intent.EXTRA_EMAIL, new String[]{eTo.getText().toString()});
+            it.putExtra(Intent.EXTRA_SUBJECT,eSubject.getText().toString());
+            it.putExtra(Intent.EXTRA_TEXT,eMsg.getText());
+            it.setType("message/rfc822");
+            startActivity(Intent.createChooser(it,"Choose Mail App"));
+        });
+        voice_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voice_to_text();
+            }
+        });
+
+    }
+
+    private void voice_to_text() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speech to Text \n Say Something!!");
+        try {
+            startActivityForResult(intent, VOICE_CODE);
+        } catch (ActivityNotFoundException e) {
+
+        }
+    }
+
+    // receive voice input and set it to textMessage
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case VOICE_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    eMsg.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+}
